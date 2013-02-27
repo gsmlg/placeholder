53fc11a7f43a3a7925316a96f797e94eaef3c2ce
George <wukongsoul@gmail.com>
2013-02-27 18:07:46 +0800

    css

--- placeholder.js
+++ placeholder.js
@@ -1,8 +1,20 @@
 /**
  * @Author Shiming
+ * @email gaoshiming@live.com
  * @time 2012-12-21 10:45:49
  */
 (function(){
+
+function getCssEffect(domObj) {
+    if (domObj.currentStyle) {
+        return domObj.currentStyle;
+    } else if (window.getComputedStyle) {
+        return document.defaultView.getComputedStyle( domObj );
+    } else {
+        return null
+    }
+}
+
 var EventUtil = {
 	addHandler: function(element, type, handler){
 		if (element.addEventListener){
@@ -42,25 +54,34 @@ EventUtil.addHandler(document, 'readystatechange', function(){
 				var span = document.createElement('span');
 				// span.className = 'ui-placeholder';
 				var input = hasPlaceholder[i];
-				span.height = input.height;
-				span.width = input.width;
+				span.height = input.clientHeight + 'px';
+				span.width = input.clientWidth + 'px';
 				span.innerHTML = input.getAttribute('placeholder');
 				if (input === input.parentNode.lastChild) {
 					input.parentNode.appendChild(span);
 				} else {
 					input.parentNode.insertBefore(span, input.nextSibling);
 				}
+				var cssStyle = getCssEffect(input);
 				span.style.color = "#999";
-				span.style.textIndent = '0.5em';
+				span.style.fontSize = cssStyle.fontSize;
+				span.style.textIndent = cssStyle.textIndent;
+				span.style.lineHeight = input.clientHeight + 'px';
+				span.style.textAlign = cssStyle.textAlign;
+				span.style.paddingLeft = cssStyle.borderLeftWidth;
+				span.style.paddingTop = cssStyle.borderTopWidth;
+				span.style.paddingRight = cssStyle.borderRightWidth;
+				span.style.paddingBottom = cssStyle.borderBottomWidth;
 				span.style.position = "absolute";
 				span.style.top = input.offsetTop;
 				span.style.left = input.offsetLeft;
+				span.style.float = 'left';
 				holders[i] = span;
 			}
 			var togglePlaceholder = function(event) {
 				event = EventUtil.getEvent(event);
 				var target = EventUtil.getTarget(event);
-				// alert(target.value)
+				
 				for( var i in hasPlaceholder) {
 					if (target === hasPlaceholder[i]) {
 						if (hasPlaceholder[i].value != '') {
@@ -80,7 +101,7 @@ EventUtil.addHandler(document, 'readystatechange', function(){
 			EventUtil.addHandler(document, 'keyup', function(event) {
 				event = EventUtil.getEvent(event);
 				var target = EventUtil.getTarget(event);
-				// alert(target.value)
+				
 				for( var i in hasPlaceholder) {
 					if (target === hasPlaceholder[i]) {
 						if (hasPlaceholder[i].value != '') {
